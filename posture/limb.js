7d816ae10c111ae761a106dc23eb8d6f8e94d175
Pierre Depaz <pierre.depaz@gmail.com>
2016-02-09 16:05:29 -0500

    fine tuned y position of limbs

--- posture/limb.js
+++ posture/limb.js
@@ -12,9 +12,9 @@ var Limb = function(_joint1, _joint2){
 
 	this.display = function(){
 		line(this.pos1.x, this.pos1.y, this.pos2.x, this.pos2.y);
-		strokeWeight(1+noise(millis()*0.01, this.pos2.x)*2);
+		strokeWeight(1+noise(millis()*0.01, this.pos2.x));
 		for(var i = 0; i < 10; i++){
-			line(this.pos1.x+(noise(i, millis()*0.0001)-0.5)*15*i*this.wither_coeff, this.pos1.y+(noise(i, millis()*0.0001)-0.5)*5*i*this.wither_coeff, this.pos2.x-(noise(i, millis()*0.01)-0.5)*5, this.pos2.y+(noise(i, millis()*0.0001)-0.5)*5*i*this.wither_coeff);
+			line(this.pos1.x+(noise(i, millis()*0.0001)-0.5)*15*i*this.wither_coeff, this.pos1.y+(noise(i-100, millis()*0.0001)-0.5)*5*i*this.wither_coeff, this.pos2.x-(noise(i, millis()*0.01)-0.5)*5, this.pos2.y+(noise(i, millis()*0.0001)-0.5)*5*i*this.wither_coeff);
 		}
 	}
 
